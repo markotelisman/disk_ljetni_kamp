fa771a1c19768f5f0bd458191657260871e38712
Marko <marko.telisman@consultem.hr>
2024-08-29 13:47:35 +0200

    up

--- index.js
+++ index.js
@@ -112,8 +112,38 @@ function dohvatiVic(){
         }
     });
 }
-dohvatiVic();
+//dohvatiVic();
 
+function dohvatiMisaoDana(){
+    let oMisaoDana = "";
+    oMisaoDana = document.getElementById("misao_dana");
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => {
+             
+            let advice = data.slip.advice;
+            //console.log(`Advice: ${advice}`);
+            oMisaoDana.innerHTML= "<br>"+advice;
+    }); 
+}
+dohvatiMisaoDana();
+
+function prevediMisaoDana (){
+    const res = fetch("https://libretranslate.com/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: "",
+            source: "auto",
+            target: "cro",
+            format: "text",
+            alternatives: 3,
+            api_key: ""
+        }),
+        headers: { "Content-Type": "application/json" }
+    });
+    
+    console.log(res.json());
+}
 function dohvatiMacu(){
     let sImgElement = "";
     let sUrl = "";
@@ -122,16 +152,38 @@ function dohvatiMacu(){
     oMacaDana = document.getElementById("maca_element");
      
 
-    for (brojac = 1; brojac <5; brojac++)
+    for (brojac = 0; brojac < 4; brojac++)
     {
         fetch('https://api.thecatapi.com/v1/images/search')
         .then(response => response.json())
         .then(data => {
             sUrl = data[0].url;
-            sImgElement += "<img class='img-responsive img-circle margin image-tilt image-grayscale bounce pulse fade-in-out wobble' src='"+sUrl+"' style='border-radius:15%; width:300px;height:auto; cursor:pointer;'   />";
+            //CSS Klase: image-tilt image-grayscale bounce pulse fade-in-out wobble flip shrink-grow heartbeat glow
+            let sKlase = "";
+            sImgElement += "<img class='img-responsive img-circle margin "+sKlase+"' src='"+sUrl+"' style='border-radius:15%; width:300px;height:auto; cursor:pointer;'   />";
             oMacaDana.innerHTML = sImgElement;
       });
     }
     
 }
 dohvatiMacu();
+
+async function dohvatiVrijeme(){
+    const url = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5&units=imperial&lang=en';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '0c598a9abamsh11fe10efa38aa22p1ea192jsn12a94d1c68d4',
+            'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+//dohvatiVrijeme();
